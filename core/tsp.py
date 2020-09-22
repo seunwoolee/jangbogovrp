95ab32b9415665b8e96d4ef4135c4debfc05884a
seungwoo <leemoney93@naver.com>
2020-09-23 08:57:10 +0900

    무게 기준

--- core/tsp.py
+++ core/tsp.py
@@ -1,24 +1,36 @@
 from __future__ import print_function
-
+from typing import List, Tuple
 from datetime import date
 
-from django.db.models import Q
+from django.db.models import Q, Sum
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
 from customer.models import Order, MutualDistance, Customer
+
+Customers = List[Tuple]
 
 
 def create_data_model(code: str, delivery_date: date):
     """Stores the data for the problem."""
     data = {}
-    customers: list = list(Order.objects.values_list('customer', flat=True).filter(
-        Q(date=delivery_date), Q(company__code=code), Q(is_am=True)).distinct())
-    starting_position: Customer = Customer.objects.filter(customer_id='admin').first() # TODO 하드코딩
-    customers.insert(0, starting_position.id)
+    customers: Customers = list(Order.objects.values_list('customer').filter(
+        Q(date=delivery_date), Q(company__code=code), Q(is_am=True)).annotate(test=Sum('price')))
+
+    customers_ids: list = []
+    customers_prices: list = []
+
+    for customer in customers:
+        customers_ids.append(customer[0])
+        customers_prices.append(customer[1])
+
+    starting_position: Customer = Customer.objects.filter(customer_id='admin').first()  # TODO 하드코딩
+    customers_ids.insert(0, starting_position.id)
+    customers_prices.insert(0, 0)
+    each_price = round(sum(customers_prices) / 4)
     temp: list = []
-    for start in customers:
+    for start in customers_ids:
         from_to_arr: list = []
-        for end in customers:
+        for end in customers_ids:
             if start == end:
                 from_to_arr.append(0)
                 continue
@@ -27,51 +39,45 @@ def create_data_model(code: str, delivery_date: date):
             from_to_arr.append(distance)
         temp.append(from_to_arr)
 
+
     data['distance_matrix'] = temp
+    data['demands'] = customers_prices
+    data['vehicle_capacities'] = [each_price+10000, each_price+10000, each_price+10000, each_price+10000]
     data['num_vehicles'] = 4
     data['depot'] = 0
+
     return data
 
 
 def print_solution(data, manager, routing, solution):
     """Prints solution on console."""
-    max_route_distance = 0
+    total_distance = 0
+    total_load = 0
     for vehicle_id in range(data['num_vehicles']):
         index = routing.Start(vehicle_id)
         plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
         route_distance = 0
+        route_load = 0
         while not routing.IsEnd(index):
-            plan_output += ' {} -> '.format(manager.IndexToNode(index))
+            node_index = manager.IndexToNode(index)
+            route_load += data['demands'][node_index]
+            plan_output += ' {0} Load({1}) -> '.format(node_index, route_load)
             previous_index = index
             index = solution.Value(routing.NextVar(index))
             route_distance += routing.GetArcCostForVehicle(
                 previous_index, index, vehicle_id)
-        plan_output += '{}\n'.format(manager.IndexToNode(index))
+        plan_output += ' {0} Load({1})\n'.format(manager.IndexToNode(index),
+                                                 route_load)
         plan_output += 'Distance of the route: {}m\n'.format(route_distance)
+        plan_output += 'Load of the route: {}\n'.format(route_load)
         print(plan_output)
-        max_route_distance = max(route_distance, max_route_distance)
-    print('Maximum of the route distances: {}m'.format(max_route_distance))
-
-
-# def print_solution(manager, routing, solution):
-#     """Prints solution on console."""
-#     print('Objective: {} miles'.format(solution.ObjectiveValue()))
-#     index = routing.Start(0)
-#     plan_output = 'Route for vehicle 0:\n'
-#     route_distance = 0
-#     while not routing.IsEnd(index):
-#         plan_output += ' {} ->'.format(manager.IndexToNode(index))
-#         previous_index = index
-#         index = solution.Value(routing.NextVar(index))
-#         route_distance += routing.GetArcCostForVehicle(previous_index, index, 0)
-#     plan_output += ' {}\n'.format(manager.IndexToNode(index))
-#     print(plan_output)
-#     plan_output += 'Route distance: {}miles\n'.format(route_distance)
+        total_distance += route_distance
+        total_load += route_load
+    print('Total distance of all routes: {}m'.format(total_distance))
+    print('Total load of all routes: {}'.format(total_load))
 
 
 def tsp(data: dict):
-    # data = create_data_model()
-    # Create the routing index manager.
     manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
                                            data['num_vehicles'], data['depot'])
 
@@ -88,16 +94,22 @@ def tsp(data: dict):
     # Define cost of each arc.
     routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
 
-    # Add Distance constraint.
-    dimension_name = 'Distance'
-    routing.AddDimension(
-        transit_callback_index,
-        0,  # no slack
-        99999999,  # vehicle maximum travel distance
+
+    # Add Capacity constraint.
+    def demand_callback(from_index):
+        """Returns the demand of the node."""
+        # Convert from routing variable Index to demands NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        return data['demands'][from_node]
+
+    demand_callback_index = routing.RegisterUnaryTransitCallback(
+        demand_callback)
+    routing.AddDimensionWithVehicleCapacity(
+        demand_callback_index,
+        0,  # null capacity slack
+        data['vehicle_capacities'],  # vehicle maximum capacities
         True,  # start cumul to zero
-        dimension_name)
-    distance_dimension = routing.GetDimensionOrDie(dimension_name)
-    distance_dimension.SetGlobalSpanCostCoefficient(100)
+        'Capacity')
 
     # Setting first solution heuristic.
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
